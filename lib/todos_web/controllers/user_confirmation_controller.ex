73f6625e47332195905745f30aba62ce1e4ea2f5
Aryan Iyappan <aryaniyappan2006@gmail.com>
2023-11-13 07:47:58 +0530

    add auth system

---
+++ lib/todos_web/controllers/user_confirmation_controller.ex
@@ -0,0 +1,44 @@
+defmodule TodosWeb.UserConfirmationController do
+  use TodosWeb, :controller
+
+  alias Todos.Accounts
+
+  def create(conn, %{"user" => %{"email" => email}}) do
+    if Accounts.get_user_by_email(email) do
+      # If the email is found, send the confirmation instructions.
+      Accounts.deliver_user_confirmation_instructions(
+        user,
+        &confirmation_url(conn, &1)
+      )
+    end
+
+    # Respond with a generic message either way to avoid email enumeration
+    json(conn, %{message: "If your email is in our system and it has not been confirmed yet, you will receive an email with instructions shortly."})
+  end
+
+  def update(conn, %{"token" => token}) do
+    case Accounts.confirm_user(token) do
+      {:ok, _} ->
+        # Return a success message for a valid confirmation
+        json(conn, %{message: "User confirmed successfully."})
+
+      :error ->
+        # Handle the error case differently depending on whether the user is signed in
+        current_user = conn.assigns[:current_user]
+        if current_user && current_user.confirmed_at do
+          # No need to send a response as the user is already confirmed.
+          json(conn, %{message: "User already confirmed."})
+        else
+          # If the token is invalid or expired, return an error message
+          conn
+          |> put_status(:unprocessable_entity)
+          |> json(%{error: "User confirmation link is invalid or it has expired."})
+        end
+    end
+  end
+
+  # Helper function to generate the confirmation URL
+  defp confirmation_url(conn, token) do
+    TodosWeb.Endpoint.url(conn) <> "users/confirm/#{token}"
+  end
+end
--- lib/todos_web/router.ex
+++ lib/todos_web/router.ex
@@ -1,12 +1,32 @@
 defmodule TodosWeb.Router do
   use TodosWeb, :router
 
+  import TodosWeb.UserAuth
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/api", TodosWeb do
     pipe_through :api
+
+    ## Authentication routes
+
+    post "/users/register", UserRegistrationController, :create
+    post "/users/log_in", UserSessionController, :create
+    post "/users/reset_password", UserResetPasswordController, :create
+    put "/users/reset_password/:token", UserResetPasswordController, :update
+  end
+
+  scope "/api", TodosWeb do
+    pipe_through [:api, :require_api_user]
+
+    put "/users/settings", UserSettingsController, :update
+    get "/users/settings/confirm_email/:token", UserSettingsController, :confirm_email
+
+    delete "/users/log_out", UserSessionController, :delete
+    post "/users/confirm", UserConfirmationController, :create
+    post "/users/confirm/:token", UserConfirmationController, :update
   end
 
   # Enable Swoosh mailbox preview in development
